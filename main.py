355310f482294445d0c35e93b93b2c984cb65327
Paul Fannon <lurchworld@gmail.com>
2018-03-28 15:16:50 -0500

    Made a TODOs template with dynamic list

--- main.py
+++ main.py
@@ -22,4 +22,16 @@ def hello():
     template = jinja_env.get_template('hello_greeting.html')
     return template.render(name = first_name)
 
+tasks = []
+
+@app.route("/todos", methods = ['POST', 'GET'])
+def todos():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+    template = jinja_env.get_template('todos.html')
+    return template.render(tasks=tasks)
+
 app.run()
