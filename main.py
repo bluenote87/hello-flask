1a0ef860b74caafe583b0532a638c4d22502b39b
Paul Fannon <lurchworld@gmail.com>
2018-03-29 11:51:43 -0500

    Refactored the code to use render_template function

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 
 app = Flask(__name__)
@@ -13,19 +9,16 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    template = jinja_env.get_template('hello_form.html')
-    return template.render(title = "Hello.")
+    return render_template('hello_form.html')
 
 @app.route("/search-form")
 def search():
-    template = jinja_env.get_template('forms.html')
-    return template.render(title = "Search query for DuckDuckGo")
+    return render_template('forms.html', title = "Search query for DuckDuckGo")
 
 @app.route("/hello", methods=["POST"])
 def hello():
     first_name = request.form['first_name']
-    template = jinja_env.get_template('hello_greeting.html')
-    return template.render(title = "Allow me to welcome you!", name = first_name)
+    return render_template('hello_greeting.html', title = "Allow me to welcome you!", name = first_name)
 
 tasks = []
 
@@ -36,7 +29,6 @@ def todos():
         task = request.form['task']
         tasks.append(task)
 
-    template = jinja_env.get_template('todos.html')
-    return template.render(title="Your TODOs list",tasks=tasks)
+    return render_template('todos.html', title="Your TODOs list",tasks=tasks)
 
 app.run()
